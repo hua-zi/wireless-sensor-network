89b10711c351732883587f2ad4ec585d723d35d9
hua-zi <1217176799@qq.com>
2023-02-25 00:08:47 +0800

    2023-02-25- 0:08:46

--- 上传github仓库.bat
+++ 上传github仓库.bat
@@ -2,7 +2,7 @@ set CURRENT_DATE_TIME=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2
 
 git add .
 git commit -m "%CURRENT_DATE_TIME%"
-git push origin master
+git push origin main
    
 echo %CURRENT_DATE_TIME%  
 pause
